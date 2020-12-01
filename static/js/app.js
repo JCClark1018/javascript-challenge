0214ab3f155f8b75c613c5b638d71662ac0063d7
JCClark1018 <JCClark1018@gmail.com>
2020-12-01 13:21:02 -0600

    Filter Table button prints input to console.log, disables refresh, and clears table

--- static/js/app.js
+++ static/js/app.js
@@ -17,3 +17,27 @@ ufoData.forEach(function(ufoTable) {
       cell.text(value);
     });
   });
+
+  //button
+  var button = d3.select("#filter-btn")
+
+  //event handler on button click
+  button.on("click", buttonClick)
+
+  //create function for event
+  function buttonClick() {
+        
+    //prevent refresh and clear table per requirements
+    d3.event.preventDefault();
+    tbody.html("");
+
+    //select input and get HTML node
+    var dateInput = d3.select("#datetime");
+    
+    //get data value from each input
+    var dateData = dateInput.property("value");
+   
+    //prove it
+    console.log(dateData);
+
+}
